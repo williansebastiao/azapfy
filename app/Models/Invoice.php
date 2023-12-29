cf32c30072ef57700651e233bbaf691e3de72cec
Willians Pereira <willians@4vconnect.com>
2023-12-28 21:42:00 -0300

    update model invoice

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Invoice extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'document_code',
+        'amount',
+        'date_of_issue',
+        'document_sender',
+        'sender_name',
+        'document_transporter',
+        'transporter_name',
+        'active',
+    ];
 }

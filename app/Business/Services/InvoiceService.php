f34a73c64436b21bed07ee0b3778d24df98d3476
Willians Pereira <willians@4vconnect.com>
2023-12-31 16:58:59 -0300

    added rabbitmq

--- app/Business/Services/InvoiceService.php
+++ app/Business/Services/InvoiceService.php
@@ -59,7 +59,7 @@ class InvoiceService
             throw new InvoiceException('Ocorreu um erro ao salvar os dados', StatusCode::INTERNAL_SERVER_ERROR);
         }
 
-        Mail::to(auth()->user()->email)->send(new Invoice($invoiceDTO));
+        Mail::to(auth()->user()->email)->queue(new Invoice($invoiceDTO));
         return $saved;
     }
 
